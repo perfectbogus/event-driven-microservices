4e093a64f6b3e6b570b6bd49698a882d7ffa2474
Eduardo Escobar <jose.escobar@oracle.com>
2022-07-17 18:12:44 -0500

    Initial commit

---
+++ src/main/java/dev/perfectbogus/demo/MicroservicesDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.perfectbogus.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MicroservicesDemoApplication.class, args);
+  }
+
+}
